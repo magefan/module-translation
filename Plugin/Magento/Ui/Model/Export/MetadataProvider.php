2c5b51d84b81672aa81a0f45d6e70a7bb01b6315
Magefan <ihoronesuch@gmail.com>
2024-06-26 16:06:51 +0300

    Update MetadataProvider.php

--- Plugin/Magento/Ui/Model/Export/MetadataProvider.php
+++ Plugin/Magento/Ui/Model/Export/MetadataProvider.php
@@ -10,7 +10,7 @@ class MetadataProvider
         \Magento\Ui\Model\Export\MetadataProvider $subject,
         $result
     ) {
-        $id = trim(str_replace('"', '', $result[0]));
+        $id = trim(str_replace('"', '', $result[0] ?? ''));
         if ('ID' == $id && 'Original Text' == trim($result[1]) && 'Translate Text' ==  trim($result[3])) {
             $result = [
                 'key_id',
